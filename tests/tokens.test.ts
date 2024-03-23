2a07b1ff8c527c3284e1a27a97b6a3db1863989a
friedger <mail@friedger.de>
2024-03-23 16:35:29 +0100

    chore: add sip10 tests

---
+++ tests/tokens.test.ts
@@ -0,0 +1,18 @@
+import { Cl } from '@stacks/transactions';
+import { beforeEach, describe, expect } from 'vitest';
+import { createClientMockSetup } from './clients/mock-client';
+import { sip10Tests } from './clients/sip10-client.ts';
+const { goToNextCycle, requestMint, prepareTest, contracts, bot } = createClientMockSetup();
+const mintDelay = 14;
+
+describe('lisa token', () => {
+  beforeEach(() => {
+    prepareTest().map(r => expect(r.result).toBeOk(Cl.bool(true)));
+    requestMint(100e6);
+    goToNextCycle();
+    simnet.mineEmptyBlocks(mintDelay);
+    simnet.callPublicFn(contracts.rebase1, 'finalize-mint', [Cl.uint(1)], bot);
+  });
+
+  sip10Tests(contracts.lqstx);
+});
