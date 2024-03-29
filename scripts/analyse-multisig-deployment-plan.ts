323da8dc6c2e694e812262d8f5bc4e9219c51db2
MarvinJanssen <MarvinJanssen@users.noreply.github.com>
2024-03-29 10:40:23 +0100

    feat: analyse checks against local version

--- scripts/analyse-multisig-deployment-plan.ts
+++ scripts/analyse-multisig-deployment-plan.ts
@@ -3,20 +3,29 @@
 
 import {
 	MultiSigSpendingCondition,
+	PayloadType,
 	StacksMessageType,
 	deserializeTransaction,
 } from "@stacks/transactions";
-import { assertSigner, readPlan } from "./utils.ts";
-import { getStacksAddress, getStacksPubkeys } from "./config.ts";
+import { printUnifiedDiff } from 'print-diff';
+import { assertSigner, deployPlan, readPlan } from "./utils.ts";
+import { getStacksAddress, getStacksPubkeys, testnetAddressReplacements } from "./config.ts";
 import { inspect } from "util";
+
+const abortOnMissingDeploy = true;
+const abortOnMismatchedDeploy = true;
+const verboseTxPrint = false;
 
 const address = getStacksAddress();
 const pubkeys = getStacksPubkeys();
 
+let errors = false;
+
 readPlan()
 	.then(plan => plan.map(tx => deserializeTransaction(tx)))
-	.then(plan => {
+	.then(async (plan) => {
 		console.log(`Plan contains ${plan.length} transactions`);
+		const localPlan = await deployPlan(testnetAddressReplacements);
 		plan.map(tx => {
 			try {
 				assertSigner(tx.auth.spendingCondition, address);
@@ -24,8 +33,31 @@ readPlan()
 			catch (error) {
 				console.log(`Transaction with txid ${tx.txid()}, signer is invalid for currently set address`);
 			}
-			console.log(inspect(tx, { showHidden: false, depth: null, colors: true }));
+			if (tx.payload.payloadType === PayloadType.SmartContract || tx.payload.payloadType === PayloadType.VersionedSmartContract) {
+				const { contractName, codeBody } = tx.payload;
+				const localDeploy = localPlan.find(item => item.contractName === contractName.content);
+				if (localDeploy === undefined) {
+					console.warn(`There is a contract deploy for '${contractName.content}' but there is no local version.`);
+					if (abortOnMissingDeploy)
+						printUnifiedDiff(codeBody.content, "");
+					errors = true;
+				}
+				if (codeBody.content !== localDeploy?.codeBody) {
+					console.warn(`Code body of contract deploy '${contractName.content}' does not match local version. Are you on the right commit?`);
+					if (abortOnMismatchedDeploy)
+						printUnifiedDiff(codeBody.content, localDeploy?.codeBody as string);
+					errors = true;
+				}
+			}
+			if (verboseTxPrint)
+				console.log(inspect(tx, { showHidden: false, depth: null, colors: true }));
 			const missingSigs = pubkeys.length - (tx.auth.spendingCondition as MultiSigSpendingCondition).fields.reduce((sum, field) => sum + (field.contents.type === StacksMessageType.MessageSignature ? 1 : 0), 0);
-			console.log(missingSigs === 0 ? 'Transaction is fully signed' : `Transaction needs ${missingSigs} signature(s)`);
+			console.log(missingSigs === 0 ? `Transaction ${tx.txid()} is fully signed` : `Transaction ${tx.txid()} needs ${missingSigs} signature(s)`);
 		});
+	}).then(() => {
+		if (errors) {
+			console.error("Errors were found in the plan.");
+			process.exit(1);
+		}
+		console.log("Plan analysed successfully");
 	});
